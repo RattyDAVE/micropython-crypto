c40ecfda6ab48b463bfb546a29a6a8477b1dce4b
RattyDAVE <dave@pucknell.co.uk>
2023-04-17 20:35:57 +0100

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,5 @@
+import os
+
 def wget(url, filename):
     from urequests import get
     r = get(url)
@@ -5,13 +7,18 @@ def wget(url, filename):
     with open(filename, 'wb') as f:
         f.write(r.content)
  
-import os
-
-
 # Install Webserver
 if not 'www' in os.listdir(): os.mkdir("/www")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebTemplate.py", "microWebTemplate.py")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebSrv.py", "microWebSrv.py")
+
+#Sample Files
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/index.html","www/index.html")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/test.pyhtml","www/test.pyhtml")
+
+#Install the running files
+if not 'log' in os.listdir(): os.mkdir("/log")
+wget("https://raw.githubusercontent.com/RattyDAVE/micropython-crypto/master/boot.py","boot.py")
+wget("https://raw.githubusercontent.com/RattyDAVE/micropython-crypto/master/main.py","main.py")
+wget("https://raw.githubusercontent.com/RattyDAVE/micropython-crypto/master/functions.py","functions.py")
 
