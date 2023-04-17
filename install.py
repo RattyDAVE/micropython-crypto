eb325b1dfb8b6caeea3caa44802bda59ddaa7500
RattyDAVE <dave@pucknell.co.uk>
2023-04-17 10:46:57 +0100

    Update install.py

--- install.py
+++ install.py
@@ -6,8 +6,12 @@ def wget(url, filename):
         f.write(r.content)
  
 import os
-#os.mkdir("/www")    
+
+
+# Install Webserver
+if not 'www' in os.listdir(): os.mkdir("/www")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebTemplate.py", "microWebTemplate.py")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebSrv.py", "microWebSrv.py")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/index.html","www/index.html")
 wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/test.pyhtml","www/test.pyhtml")
+
