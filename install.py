7cf73e864d15a5138e25de010f15a40374c34fc9
RattyDAVE <dave@pucknell.co.uk>
2023-04-17 09:31:59 +0100

    Create install.py

---
+++ install.py
@@ -0,0 +1,13 @@
+def wget(url, filename):
+    from urequests import get
+    r = get(url)
+    print(r.content)
+    with open(filename, 'wb') as f:
+        f.write(r.content)
+ 
+import os
+#os.mkdir("/www")    
+wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebTemplate.py", "microWebTemplate.py")
+wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebSrv.py", "microWebSrv.py")
+wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/index.html","www/index.html")
+wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/test.pyhtml","www/test.pyhtml")
