067aeb50c0b38049aeeda2e9fa44c160d805ca90
RattyDAVE <dave@pucknell.co.uk>
2023-06-18 16:48:58 +0100

    Update install.py

--- install.py
+++ install.py
@@ -7,13 +7,13 @@ def wget(url, filename):
         f.write(r.content)
  
 # Install Webserver
-if not 'www' in os.listdir(): os.mkdir("/www")
-wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebTemplate.py", "microWebTemplate.py")
-wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebSrv.py", "microWebSrv.py")
+#if not 'www' in os.listdir(): os.mkdir("/www")
+#wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebTemplate.py", "microWebTemplate.py")
+#wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/microWebSrv.py", "microWebSrv.py")
 
 #Sample Files
-wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/index.html","www/index.html")
-wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/test.pyhtml","www/test.pyhtml")
+#wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/index.html","www/index.html")
+#wget("https://raw.githubusercontent.com/jczic/MicroWebSrv/master/www/test.pyhtml","www/test.pyhtml")
 
 #Install the running files
 if not 'logs' in os.listdir(): os.mkdir("/logs")
@@ -21,3 +21,6 @@ wget("https://raw.githubusercontent.com/RattyDAVE/micropython-crypto/master/boot
 wget("https://raw.githubusercontent.com/RattyDAVE/micropython-crypto/master/main.py","main.py")
 wget("https://raw.githubusercontent.com/RattyDAVE/micropython-crypto/master/functions.py","functions.py")
 
+#Install Libs
+if not 'lib' in os.listdir(): os.mkdir("/lib")
+wget("https://raw.githubusercontent.com/RattyDAVE/EPD_2in13_B_V4_Landscape/main/EPD_2in13_B_V4_Landscape.py","lib/EPD_2in13_B_V4_Landscape.py")
